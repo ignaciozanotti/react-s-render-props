30f9aa1c6e0a04d5feb1e002077f8ac65484f18d
ignaciozanotti <ignacio.zanotti@gmail.com>
2020-04-27 04:00:39 -0300

    custom thanks

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ class MouseTracker extends React.Component {
   render() {
     return (
       <div>
-        <h1>Move the mouse around!</h1>
+        <h1>Gracias Maxi!!</h1>
         <Mouse render={mouse => (
           <Cat mouse={mouse} />
         )}/>
